571cc9533fab77500c647e01ad5b07ac0038a8d8
Braden Mosier <bmosi77@tscstudents.net>
2026-01-31 14:03:48 -0500

    basic shoot for Noelle for her to made a shooter command

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,4 +6,9 @@ public class Constants {
         public static final int ENCODER_PORT = 0;
         public static final double TURRETRATIO = 11;
     }
+    public static final class Shooter {
+        public static final int MOTOR_LEFT_PORT = 41;
+        public static final int MOTOR_RIGHT_PORT = 42;
+        public static final int MOTOR_HOOD_PORT = 40;
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import com.ctre.phoenix6.hardware.TalonFXS;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXSConfiguration;
+import com.ctre.phoenix6.configs.TalonFXSConfigurator;
+import com.ctre.phoenix6.signals.MotorArrangementValue;
+
+public class Shooter extends SubsystemBase {
+  /** Creates a new Shooter. */
+  // idk what to caller the motors of the shooter
+  // shooting dir is froward.
+  private TalonFXS motorLeft;
+  private TalonFXS motorRight;
+  private TalonFXS motorHood;
+  private DutyCycleOut dutyCycle = new DutyCycleOut(0);
+  
+  public Shooter() {
+
+    TalonFXS motorLeft = new TalonFXS(Constants.Shooter.MOTOR_LEFT_PORT);
+    TalonFXS motorRight = new TalonFXS(Constants.Shooter.MOTOR_RIGHT_PORT);
+    TalonFXS motorHood = new TalonFXS(Constants.Shooter.MOTOR_HOOD_PORT);
+
+    // the config for all the motors we should do differnt ones
+    TalonFXSConfiguration config = new TalonFXSConfiguration();
+
+    config.Voltage
+      .withPeakForwardVoltage(12)
+      .withPeakReverseVoltage(-12);
+    
+    config.Commutation.MotorArrangement = MotorArrangementValue.Minion_JST;
+    motorHood.getConfigurator().apply(config);
+    motorLeft.getConfigurator().apply(config);
+    motorRight.getConfigurator().apply(config);
+  }
+
+  public double shoot(double power){
+    dutyCycle.Output = power;
+    motorLeft.setControl(dutyCycle);
+    motorRight.setControl(dutyCycle);
+    return power;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
