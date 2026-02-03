d4c38a865b4e243c978b1ee0b7a64efca9761173
Mark Schneider <schne112@purdue.edu>
2026-02-02 20:43:55 -0500

    rename commands folder

--- src/main/java/frc/robot/commands/TurretGoToAngle.java
+++ src/main/java/frc/robot/commands/TurretGoToAngle.java
@@ -2,23 +2,18 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Commands;
+package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Turret;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class TurretAimToPose extends Command {
+public class TurretGoToAngle extends Command {
   private Turret turret;
   private double angle;
-  private Pose2d TargetPose;
-  private Pose2d BotPose;
-
-  public TurretAimToPose(Turret turret, Pose2d botPose, Pose2d targetPose) {
+  public TurretGoToAngle(Turret turret, double angle) {
     this.turret = turret;
-    this.TargetPose = TargetPose;
-    this.BotPose = BotPose;
+    this.angle = angle;
     addRequirements(turret);
   }
 
@@ -27,7 +22,7 @@ public class TurretAimToPose extends Command {
 
   @Override
   public void execute() {
-    System.out.println(turret.aimAtPose(this.BotPose,this.TargetPose));
+    turret.goToAngle(angle);
   }
 
   @Override
@@ -37,6 +32,6 @@ public class TurretAimToPose extends Command {
 
   @Override
   public boolean isFinished() {
-    return  0 == turret.aimAtPose(this.BotPose,this.TargetPose);
+    return false;
   }
 }
--- src/main/java/frc/robot/commands/TurretMove.java
+++ src/main/java/frc/robot/commands/TurretMove.java
@@ -2,18 +2,20 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Commands;
+package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Turret;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class TurretGoToAngle extends Command {
+public class TurretMove extends Command {
   private Turret turret;
-  private double angle;
-  public TurretGoToAngle(Turret turret, double angle) {
+  private double speed;
+
+  public TurretMove(Turret turret,double speed) {
     this.turret = turret;
-    this.angle = angle;
+    this.speed = speed;
     addRequirements(turret);
   }
 
@@ -22,7 +24,7 @@ public class TurretGoToAngle extends Command {
 
   @Override
   public void execute() {
-    turret.goToAngle(angle);
+    turret.basicSpin(speed);
   }
 
   @Override
